c52be8b700e8fa78e2b3466e946bd5abeb0e1631
KrajAnna <krajanna2505@gmail.com>
2024-10-02 14:53:48 +0200

    first commit

---
+++ src/main/java/org/anna/WorldClock/WorldClockApplication.java
@@ -0,0 +1,13 @@
+package org.anna.WorldClock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldClockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/anna/WorldClock/WorldClockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anna.WorldClock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
